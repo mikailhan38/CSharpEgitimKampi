a75eaed580021cc9c1f430438bf1f165fecb4509
mikail lale <mikail.lale@outlook.com>
2024-10-03 23:54:38 +0300

    Add project files.

---
+++ 01_CharpEgitimi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _01_CharpEgitimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //int yearOfBirth;
+            //int year = DateTime.Now.Year;
+            //int yas;
+            //string name;
+            //Console.WriteLine("Lütfen adınızı yazınız");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Lütfen doğum yılını giriniz");
+            //yearOfBirth = Convert.ToInt32(Console.ReadLine());
+            //yas= year-yearOfBirth;
+            //Console.WriteLine("Sayın "+name+" Yaşınız :"+yas);
+            //Console.ReadLine();
+
+        }
+    }
+}
---
+++ SqliteDeneme/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.Sqlite;
+
+Console.WriteLine("Hello, World!");
+using (SqliteConnection connection = new SqliteConnection("Data Source=ogrenci.db"))
+{
+    connection.Open();
+
+    string selectSql = "SELECT * FROM kullanicilar";
+
+    using (SqliteCommand command = new SqliteCommand(selectSql, connection))
+    {
+        using (SqliteDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string ad = reader.GetString(1);
+                string soyad = reader.GetString(2);
+                string email = reader.GetString(3);
+                string kayitTarihi = reader.GetString(4);
+
+                Console.WriteLine($"ID: {id}, Ad: {ad}, Soyad: {soyad}, Email: {email}, Kayıt Tarihi: {kayitTarihi}");
+            }
+        }
+    }
+}
+Console.WriteLine("Bağlantı Başarılı");
